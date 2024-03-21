1f415527ed4ee144dfec00e46246c682935b0529
Jon Fairbanks <fairplay89@gmail.com>
2024-03-20 22:51:07 -0700

    Re-work directory structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: "/fairbanks-io",
-    output: "export",  // Enable static exports
+    output: "export", // Enable static exports
     reactStrictMode: true,
 };
 
